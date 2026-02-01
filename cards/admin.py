a45257810360e0318b042ae75852c08880f235c1
vitorpixel-6436 <vitorleitye6436@gmail.com>
2026-02-01 20:19:22 +0000

    🔧 Fix: исправление admin.py - удалён импорт несуществующей модели Preset

--- cards/admin.py
+++ cards/admin.py
@@ -1,30 +1,25 @@
 from django.contrib import admin
-from .models import PCBuild, Preset
+from .models import PCBuild, GeneratedCard
 
 
 @admin.register(PCBuild)
 class PCBuildAdmin(admin.ModelAdmin):
-    list_display = ['cpu', 'gpu', 'price', 'style', 'created_at']
-    list_filter = ['style', 'created_at']
-    search_fields = ['cpu', 'gpu', 'motherboard']
-    readonly_fields = ['created_at', 'updated_at']
+    """Админка для конфигураций ПК"""
+    
+    list_display = ('name', 'cpu', 'gpu', 'price', 'warranty_months', 'created_at')
+    list_filter = ('created_at', 'warranty_months')
+    search_fields = ('name', 'cpu', 'gpu')
+    readonly_fields = ('created_at', 'updated_at')
     
     fieldsets = (
-        ('Фотография', {
+        ('Основная информация', {
+            'fields': ('name', 'price', 'warranty_months')
+        }),
+        ('Компоненты', {
+            'fields': ('cpu', 'gpu', 'ram', 'storage', 'motherboard', 'psu', 'case', 'cooling')
+        }),
+        ('Фото', {
             'fields': ('photo',)
-        }),
-        ('Основные характеристики', {
-            'fields': ('cpu', 'gpu', 'ram', 'storage')
-        }),
-        ('Дополнительные компоненты', {
-            'fields': ('motherboard', 'psu', 'case'),
-            'classes': ('collapse',)
-        }),
-        ('Цена и бонусы', {
-            'fields': ('price', 'bonuses')
-        }),
-        ('Оформление', {
-            'fields': ('style', 'generated_card')
         }),
         ('Метаданные', {
             'fields': ('created_at', 'updated_at'),
@@ -33,7 +28,24 @@ class PCBuildAdmin(admin.ModelAdmin):
     )
 
 
-@admin.register(Preset)
-class PresetAdmin(admin.ModelAdmin):
-    list_display = ['name', 'cpu', 'gpu']
-    search_fields = ['name', 'cpu', 'gpu']
+@admin.register(GeneratedCard)
+class GeneratedCardAdmin(admin.ModelAdmin):
+    """Админка для сгенерированных карточек"""
+    
+    list_display = ('build', 'style', 'card_number', 'created_at')
+    list_filter = ('style', 'created_at', 'card_number')
+    search_fields = ('build__name', 'build__cpu', 'build__gpu')
+    readonly_fields = ('created_at',)
+    
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('build', 'style', 'card_number')
+        }),
+        ('Изображение', {
+            'fields': ('image',)
+        }),
+        ('Метаданные', {
+            'fields': ('created_at',),
+            'classes': ('collapse',)
+        }),
+    )
